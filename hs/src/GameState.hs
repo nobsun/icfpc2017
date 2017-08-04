92a3f3af8ae61add3fe429fc232f6296cfa785bc
Nobuo Yamashita <nobsun@sampou.org>
2017-08-05 08:11:42 +0900

    add GameState.hs

---
+++ hs/src/GameState.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module GameState where
+
+import GHC.Generics
+import Data.Aeson
+import Data.Text
+import Data.Graph.Inductive.Basic
+-- import Data.Graph.Inductive.Example (genUNodes,genLNodes,labUEdges,noEdges)
+import Data.Graph.Inductive.Graph
+import Data.Graph.Inductive.PatriciaTree
+
+import Online (Setup(..))
+import InternalJSON
+
+data GameState = GameState
+  { punter :: PunterId
+  , punters :: Int
+  , graph :: Gr SiteLabel RiverLabel
+  } deriving (Generic, Show)
+
+type SiteLabel = Bool
+type RiverLabel = PunterId
+
+nonClaimed :: RiverLabel
+nonClaimed = -1
+
+mine :: SiteLabel
+mine = True
+
+initGameState :: Setup -> GameState
+initGameState (Setup spunter spunters smap) = GameState
+  { punter = spunter
+  , punters = spunters
+  , graph = inigraph smap
+  }
+
+inigraph :: Map -> Gr SiteLabel RiverLabel
+inigraph (Map ss rs ms) = mkGraph (Prelude.map (lnode ms . deSite) ss) (Prelude.map (ledge nonClaimed . deRiver) rs)
+
+deSite :: Site -> SiteId
+deSite (Site i) = i
+
+deRiver :: River -> (SiteId, SiteId)
+deRiver (River s t) = (s, t)
+
+lnode :: [SiteId] -> SiteId -> LNode SiteLabel
+lnode ms si
+  | si `elem` ms = (si, True)
+  | otherwise    = (si, False)
+
+ledge :: RiverLabel -> (SiteId, SiteId) -> LEdge RiverLabel
+ledge lab (s,t) = (s,t,lab)
