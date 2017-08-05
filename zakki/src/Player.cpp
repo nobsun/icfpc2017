7d1a5047073855bb4c0173da4afa40ec1ca20d78
Kensuke Matsuzaki <zakki@peppermint.jp>
2017-08-05 19:42:18 +0900

    Maximize next turn score

--- zakki/src/Player.cpp
+++ zakki/src/Player.cpp
@@ -26,12 +26,32 @@ JMove genmove(Game& game, int player) {
     if (game.owner[i] < 0)
       openRivers.push_back(i);
   }
-  cerr << "SCORE:" << game.score(player) << endl;
+  int score0 = game.score(player);
+  cerr << "SCORE:" << score0 << endl;
+  int maxScore = score0;
+  int maxMove = 0;
+  for (size_t i = 0; i < openRivers.size(); i++) {
+    Game g2(game);
+    auto& r = game.game.map.rivers[openRivers[i]];
+    JMove m = JMove::claim(player, r.source, r.target);
+    vector<JMove> ms;
+    ms.push_back(m);
+    g2.update(ms);
+    //cerr << "OWNER:" << json(g2.owner) << endl;
+    int score = g2.score(player);
+    cerr << m.source << "->" << m.target << " SCORE:" << score << endl;
+    if (score >= maxScore) {
+      maxScore = score;
+      maxMove = openRivers[i];
+    }
+  }
+
   if (openRivers.size() == 0) {
     return JMove::pass(player);
   } else {
     uniform_int_distribution<int> dist(0, openRivers.size() - 1);
-    int n = openRivers[dist(mt)];
+    //int n = openRivers[dist(mt)];
+    int n = maxMove;
     auto& r = game.game.map.rivers[n];
     return JMove::claim(player, r.source, r.target);
   }
