20512a1ecb3f09313e96597c3f211ced6e992230
Kei Hibino <ex8k.hibino@gmail.com>
2017-08-05 18:49:17 +0900

    use fail in IO.

--- hs/app/TestPunter.hs
+++ hs/app/TestPunter.hs
@@ -35,12 +35,11 @@ main = do
           input h
           str <- input h
           let pid = getPunterId str -- workaround
-          let msetup = decode (B.pack str)
-          case msetup :: Maybe Setup of
-            Just setup -> do
-              --let pid = (punter::Setup->Int) setup
-              output h (ReadyOn{ready=pid, state=Nothing, futures=Nothing} :: Ready ())
-              loop h pid
+          let msetup = decode (B.pack str) :: Maybe Setup
+          setup <- maybe (fail "TestPunter: decode failure") return msetup
+          --let pid = (punter::Setup->Int) setup
+          output h (ReadyOn{ready=pid, state=Nothing, futures=Nothing} :: Ready ())
+          loop h pid
         )
   where
     loop h pid = do
