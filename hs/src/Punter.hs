4e2a6f69e4eefd3324de42cdf7b5663645bb179c
Kei Hibino <ex8k.hibino@gmail.com>
2017-08-06 00:24:38 +0900

    add JSON instances to encode into state.

--- hs/src/Punter.hs
+++ hs/src/Punter.hs
@@ -1,10 +1,11 @@
-
+{-# LANGUAGE DeriveGeneric #-}
 module Punter where
 
-import qualified Data.Aeson as J
+import GHC.Generics (Generic)
+import Data.Aeson (ToJSON, FromJSON)
 import qualified Protocol as P
 
-class (J.ToJSON a, J.FromJSON a) => IsPunter a where
+class (ToJSON a, FromJSON a) => IsPunter a where
   setup :: P.Setup -> P.Ready a
   play  :: P.PrevMoves a -> P.MyMove a
 
@@ -12,7 +13,10 @@ data OfflineStage
   = Setup
   | GamePlay Int
   | Scoring
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON OfflineStage
+instance FromJSON OfflineStage
 
 class IsPunter a => IsOfflinePunter a where
   offlineStage :: a -> OfflineStage
