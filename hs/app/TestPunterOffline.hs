a276032bff23e73e34de8b0c5d21a4bfacc8e17d
cutsea110 <cutsea110@gmail.com>
2017-08-06 01:28:11 +0900

    greedy for offline

--- hs/app/TestPunterOffline.hs
+++ hs/app/TestPunterOffline.hs
@@ -5,13 +5,14 @@ import Data.Proxy
 import OfflinePlay
 import qualified Punter.Pass as PassPunter
 import qualified Punter.ClaimAny as AnyPunter
-import System.IO
+import qualified Punter.ClaimGreedy as GreedyPunter
 import System.Environment
 
 main :: IO ()
 main = do
-  [name, port] <- getArgs
+  [name, _port] <- getArgs
   case name of
     "pass" -> runPunterOffline (Proxy :: Proxy PassPunter.Punter)
     "any" -> runPunterOffline (Proxy :: Proxy AnyPunter.Punter)
+    "greedy" -> runPunterOffline (Proxy :: Proxy GreedyPunter.Punter)
     _ -> error "unknown punter algorithm"
