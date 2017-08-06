6e00f84f4e583c33c4301f787bfd74d48b972c95
Kei Hibino <ex8k.hibino@gmail.com>
2017-08-06 16:27:51 +0900

    add helper to resolve ambiguity.

--- hs/src/Punter/ClaimAny.hs
+++ hs/src/Punter/ClaimAny.hs
@@ -9,6 +9,7 @@ import Data.Maybe (listToMaybe)
 import Data.Set (Set, (\\))
 import qualified Data.Set as Set
 import GHC.Generics
+import Protocol (PunterId)
 import qualified Protocol as P
 import Punter
 import NormTypes (NRiver, toNRiver', deNRiver)
@@ -24,10 +25,13 @@ data Punter
 instance J.ToJSON Punter
 instance J.FromJSON Punter
 
+takePunterId :: P.Setup -> PunterId
+takePunterId = P.punter
+
 instance Punter.IsPunter Punter where
   setup s =
     P.ReadyOn
-    { P.ready   = P.punter (s :: P.Setup)
+    { P.ready   = takePunterId s
     , P.state   = Just $
         Punter
         { setupInfo = s
@@ -43,11 +47,11 @@ instance Punter.IsPunter Punter where
     , myRivers = myRivers1 `Set.union` Set.fromList [ toNRiver' s t | P.MvClaim punter' s t <- moves, punter' == punterId ]
     }
     where
-      punterId = P.punter (si :: P.Setup)
+      punterId = takePunterId si
 
   chooseMoveSimple Punter{ setupInfo = si, availableRivers = availableRivers1 } =
     case listToMaybe $ Set.toList $ availableRivers1 of
       Nothing -> P.MvPass punterId
       Just r | (s,t) <- deNRiver r -> P.MvClaim punterId s t
     where
-      punterId = P.punter (si :: P.Setup)
+      punterId = takePunterId si
