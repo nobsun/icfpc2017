5edbc4e2244f87384e4af1ad5293c725912bd859
oga <oganet2@yahoo.co.jp>
2017-08-06 21:14:51 +0900

    numbering log

--- oga/bin/lamduct.sh
+++ oga/bin/lamduct.sh
@@ -2,6 +2,7 @@
 
 NAME=$1
 PORT=$2
-rm -f ./$1.log
-/usr/local/bin/lamduct --log-level 3 --game-port $2 --client-instance-logfile ./$1.log ./$1.sh
+NUM=$3
+rm -f ./$1.$3.log
+/usr/local/bin/lamduct --log-level 3 --game-port $2 --client-instance-logfile ./$1.$3.log ./$1.sh
 
