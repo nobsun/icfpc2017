89ae2b66a951f5ecb9b76de9a7b42d702d8c1470
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-08-06 14:13:33 +0900

    continuation of 368a505259fe91a03e6f377a6b25df08e8f6310d

--- hs/src/CommonState.hs
+++ hs/src/CommonState.hs
@@ -20,7 +20,8 @@ applyMoves :: [P.Move] -> MovePool -> MovePool
 applyMoves moves (MovePool {pool = pl}) = MovePool $ foldl' upsert pl moves
 
 upsert :: IM.IntMap UF.Table -> P.Move -> IM.IntMap UF.Table
-upsert = undefined
+upsert pl (P.MvClaim p s t) = IM.insert p (UF.unify (IM.findWithDefault UF.emptyTable p pl) s t) pl
+upsert pl (P.MvPass _) = pl
 
 instance J.ToJSON MovePool
 instance J.FromJSON MovePool
