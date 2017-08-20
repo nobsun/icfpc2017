47655902ea439e66a3f65d0c38ba0ff3c28d6282
Masahiro Sakai <masahiro.sakai@gmail.com>
2017-08-20 14:48:13 +0900

    OfflinePlay: simplify message parsing

--- hs/src/OfflinePlay.hs
+++ hs/src/OfflinePlay.hs
@@ -4,8 +4,6 @@
 module OfflinePlay where
 
 import Control.Applicative
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Maybe (MaybeT (..))
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Data.Char (isDigit)
 import Data.Aeson (Result (Success, Error))
@@ -51,19 +49,28 @@ runPunterOffline' name _ =
       send (P.HandshakePunter{ P.me=name })
       (_::P.HandshakeServer) <- recv "handshake"
       jsonv <- recv "multiplex"
+      case J.fromJSON jsonv :: Result (ServerMsg a) of
+        Error _err -> fail $ "unknown messsage: " ++ show jsonv
+        Success (SMPrevMoves moves) -> (send =<< Punter.play moves)               *> loop
+        Success (SMSetup setupInfo) -> send (Punter.setup setupInfo :: P.Ready a) *> loop
+        Success (SMScoring _)       -> pure ()
 
-      (maybe (fail $ "unknown messsage: " ++ show jsonv) pure =<<) . runMaybeT $
-        ((\moves     -> lift $ (send =<< Punter.play moves)               *> loop) =<<
-          result (const empty) pure (J.fromJSON jsonv :: Result (P.PrevMoves a)))       <|>     --- check gameplay first
-        ((\setupInfo -> lift $ send (Punter.setup setupInfo :: P.Ready a) *> loop) =<<
-          result (const empty) pure (J.fromJSON jsonv :: Result P.Setup))               <|>
-        (const (pure ())                                                           =<<
-          result (const empty) pure (J.fromJSON jsonv :: Result P.Scoring))
+data ServerMsg a
+  = SMSetup P.Setup
+  | SMPrevMoves (P.PrevMoves a)
+  | SMScoring P.Scoring
+  deriving (Show)
 
-result :: (String -> b) -> (a -> b) -> Result a -> b
-result f g r = case r of
-  Error   s  ->  f s
-  Success x  ->  g x
+instance J.FromJSON a => J.FromJSON (ServerMsg a) where
+  parseJSON v
+    =   (SMPrevMoves <$> J.parseJSON v) -- check gameplay first
+    <|> (SMSetup <$> J.parseJSON v)
+    <|> (SMScoring <$> J.parseJSON v)
+
+instance J.ToJSON a => J.ToJSON (ServerMsg a) where
+  toJSON (SMSetup x) = J.toJSON x
+  toJSON (SMPrevMoves x) = J.toJSON x
+  toJSON (SMScoring x) = J.toJSON x
 
 send :: J.ToJSON a => a -> IO ()
 send x = do
